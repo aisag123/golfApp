0cc98c7d31fad26b67ce55895a6cc67b4e10fd46
unknown <aidensagaser@gmail.com>
2026-01-19 22:58:25 -0600

    added test records for testing handicap calculation started the py fast api connection

--- app/models/statsEngine.py
+++ app/models/statsEngine.py
@@ -1,5 +1,6 @@
 #created by Aiden Sagaser on 2023-10-05
 # stats engine fot the golf app
+from sqlalchemy import null
 from app.models.course import Course
 from app.models.player import Player
 
@@ -15,17 +16,33 @@ class StatsEngine:
         self.player.SetHolesPlayed(18)
         self.player.SetRoundScore(80)
 
-    # def print_summary(self):
-    #     info = self.course.GetCourseInfo()
-    #     print("player name: " + str(self.player.firstName) + " " + str(self.player.lastName))
-    #     print("course info: " + str(self.course.courseName) + " " + str(self.course.location) + " " + str(self.course.coursePar))
-    #     print("round summary: " + "holes played: " + str(self.player.holesPlayed) + " score: " + str(self.player.roundScore))
+    
+    def calculateHandicap(self, courseRating, slope, average):
+        #call function to calculate average best 8 of 20
+        if (courseRating is None or slope is None):
+            print("Course rating and slope must be provided")
+            return None
+        handicap = (average - courseRating) * 113 / slope 
+        print("Calculated handicap: ", handicap)
+        return handicap
+    
+    #this will grab from the database later
+    def calculateAverageScores(self, scores: list[int]):
+        if len(scores) < 8:
+            print("Not enough scores to calculate an accurate handicap")
+            return None
+        else:
+            sortedScores = sorted(scores)
+            bestEightScores = sortedScores[:8]
+            average = sum(bestEightScores) / 8
+            print("Average of best 8 scores: ", average)
+        return self.calculateHandicap(69.7, 125, average)
 
     # Entry point for testing
 if __name__ == "__main__":
     engine = StatsEngine()
-    engine.setupFinsihedRound()
-    engine.print_summary()
-    
+    # engine.setupFinsihedRound()
+    engine.calculateAverageScores([80, 85, 78, 90, 88, 76, 82, 79])
+
 
 
